65f843559e8bdfe957d01469fa886e0eaa0d87a8
Alexander Zarin <adamant545@gmail.com>
2023-12-08 01:02:43 +0400

    Define the create_team function

--- Mattermost-API/mm_teams_api.py
+++ Mattermost-API/mm_teams_api.py
@@ -6,3 +6,28 @@ class Teams(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/teams"
+
+    def create_team(self,
+                    name: str,
+                    display_name: str,
+                    t_type: str) -> dict:
+        """
+        Create a new team on the system.
+
+        Must be authenticated and have the create_team permission.
+
+        :param name: Unique handler for a team, will be present in the team URL.
+        :param display_name: Non-unique UI name for the team.
+        :param t_type: 'O' for open, 'I' for invite only.
+        :return: Team creation info.
+        """
+
+        url = f"{self.api_url}"
+
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('name', name)
+        self.add_to_json('display_name', display_name)
+        self.add_to_json('type', t_type)
+
+        return self.request(url, request_type='POST', body=True)
