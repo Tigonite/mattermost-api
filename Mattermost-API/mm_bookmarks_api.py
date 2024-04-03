4b16a7d00a78ff75f3ad52c939ec4a5d1d4e8f06
Alexander Zarin <adamant545@gmail.com>
2024-04-03 12:21:45 +0400

    Define the create_chnl_bkmrk function

--- Mattermost-API/mm_bookmarks_api.py
+++ Mattermost-API/mm_bookmarks_api.py
@@ -28,3 +28,50 @@ class Bookmarks(Base):
             self.add_to_json('bookmarks_since', bookmarks_since)
 
         return self.request(url, request_type='GET', body=True)
+
+    def create_chnl_bkmrk(self,
+                          channel_id: str,
+                          display_name: str,
+                          tp: str,
+                          file_id: str = None,
+                          link_url: str = None,
+                          image_url: str = None,
+                          emoji: str = None) -> dict:
+        """
+        Creates a new channel bookmark for this channel.
+
+        Minimum server version: 9.5
+
+        Must have the add_bookmark_public_channel or add_bookmark_private_channel depending on the channel type.
+        If the channel is a DM or GM, must be a non-guest member.
+
+        :param channel_id: Channel GUID.
+        :param display_name: The name of the channel bookmark.
+        :param tp: Enum: "link" "file" link for channel bookmarks that reference a link.
+        link_url is requied file for channel bookmarks that reference a file.
+        file_id is required.
+        :param file_id: The ID of the file associated with the channel bookmark.
+        Required for bookmarks of type 'file'.
+        :param link_url: The URL associated with the channel bookmark.
+        Required for bookmarks of type 'link'.
+        :param image_url: The URL of the image associated with the channel bookmark.
+        Optional, only applies for bookmarks of type 'link'.
+        :param emoji: The emoji of the channel bookmark.
+        :return: Channel Bookmarks creation info.
+        """
+
+        url = f"{self.api_url}/{channel_id}/bookmarks"
+        self.reset()
+        self.add_application_json_header()
+        self.add_to_json('display_name', display_name)
+        self.add_to_json('type', tp)
+        if file_id is not None:
+            self.add_to_json('file_id', file_id)
+        if link_url is not None:
+            self.add_to_json('link_url', link_url)
+        if image_url is not None:
+            self.add_to_json('image_url', image_url)
+        if emoji is not None:
+            self.add_to_json('emoji', emoji)
+
+        return self.request(url, request_type='POST', body=True)
