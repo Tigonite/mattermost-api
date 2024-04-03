a5cb76d4f502810a7a9f6a5203fbce4239171f31
Alexander Zarin <adamant545@gmail.com>
2024-04-03 15:35:42 +0400

    Correct the class' name

--- Mattermost-API/mm_users_api.py
+++ Mattermost-API/mm_users_api.py
@@ -2,7 +2,7 @@ from typing import Union, List, Dict
 from Mattermost_Base import Base
 
 
-class Uploads(Base):
+class Users(Base):
     def __init__(self, token: str, server_url: str):
         super().__init__(token, server_url)
         self.api_url = f"{self.base_url}/users"
